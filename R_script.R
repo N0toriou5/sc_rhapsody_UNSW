21fc6411057f882e0fc79f974b754da36f9dd7c0
N0toriou5 <49403790+N0toriou5@users.noreply.github.com>
2022-11-24 18:10:35 +0100

    Delete R_script.R

--- R_script.R
+++
@@ -1,3 +0,0 @@
-#### Code written by Daniele Mercatelli, University of Bologna
-#### This file contains the analysis considering the single run separately 
-#### (e.g. tumor and immune cells were run in different days and different captures)
